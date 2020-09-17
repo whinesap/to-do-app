33f2edac21b6946d12aafc44d806d4bfed864b7f
Madi <madison.byteme@gmail.com>
2020-09-17 13:12:25 -0700

    checkpoint completed

---
+++ app.js
@@ -0,0 +1,62 @@
+function onReady() {
+    var id = 0;
+    let toDos = [];
+    const addToDoForm = document.getElementById('addToDoForm');
+    
+    function createNewToDo() {
+        const newToDoText = document.getElementById('newToDoText');
+        if (!newToDoText.value) {return; }
+
+        toDos.push({
+            title: newToDoText.value,
+            complete: false,
+            id: id
+        });
+        id++;
+        newToDoText.value = '';
+
+        renderTheUI();
+    }
+
+    function renderTheUI() {
+        const toDoList = document.getElementById('toDoList');
+
+        toDoList.textContent = '';
+
+        toDos.forEach(function(toDo) {
+            const newLi = document.createElement('li');
+            const checkbox = document.createElement('input');
+            checkbox.type = "checkbox";
+
+            newLi.textContent = toDo.title;
+
+            const deleteBtn = document.createElement('button');
+            deleteBtn.textContent = "Delete";
+    
+            deleteBtn.addEventListener('click', function(event){
+                toDos = toDos.filter(function(item){
+                  return item.id !== toDo.id;   
+                })
+                renderTheUI();
+            })
+           
+            toDoList.appendChild(newLi);
+            newLi.appendChild(checkbox);
+            newLi.appendChild(deleteBtn);
+        });
+
+    }
+
+    addToDoForm.addEventListener('submit', event => {
+        event.preventDefault();
+        createNewToDo();
+    });
+
+    renderTheUI();
+}
+
+window.onload = function() {
+    onReady();
+};
+
+
